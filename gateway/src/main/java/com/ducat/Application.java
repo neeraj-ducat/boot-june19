391f3f8b2946833751de12292e688948fdde7832
Neeraj Kumar <nraj_adi@yahoo.co.in>
2019-08-03 16:30:28 +0530

    gateway example is added.

---
+++ gateway/src/main/java/com/ducat/Application.java
@@ -0,0 +1,17 @@
+package com.ducat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
